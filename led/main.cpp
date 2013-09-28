6a8f218962ef92f88535a4d7a93778a77c0232c2
William Yager <will.yager@gmail.com>
2013-09-28 01:30:21 -0500

    doing led stuff

--- led/main.cpp
+++ led/main.cpp
@@ -6,7 +6,16 @@ extern "C"{
 }
 
     
+void pwm(uint8_t mask, uint8_t val){
+	for (uint8_t i = 0; i < val; ++i)
+	{
+		PORTB |= mask;
+	}
+	for(uint8_t i = val; i <= 255; i++){
+		PORTB &= ~mask;
+	}
 
+}
 
 
 void setBlue(uint8_t brightness){
@@ -55,9 +64,18 @@ int main(void)
 	const uint8_t led_mask = (1<<0) | (1<<1) | (1<<4);
 	DDRB = led_mask;
 	uint8_t led_counter = 0;
-	uint8_t led_colors[] = {0, 0x2, 0x4, 0x5, 0x1};
+	//uint8_t led_colors[] = {0, 0x2, 0x4, 0x5, 0x1};
+	const uint8_t bmask = (1<<0);
+	const uint8_t gmask = (1<<1);
+	const uint8_t rmask = (1<<4);
+	const uint8_t led_masks[] = {0, bmask, gmask, bmask | rmask, rmask};
 	for(;;){
-		setAll(led_colors[led_counter]);
+		uint8_t mask = led_masks[led_counter];
+		uint8_t pwm_val = 0;
+		while(PINB & (1<<3)){
+			pwm(mask, pwm_val);
+			pwm_val++;
+		}
 		led_counter++;
 		led_counter = led_counter % 5;
 		waitForButton();
